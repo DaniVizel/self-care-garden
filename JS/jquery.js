5638fc7b9e7d8c6bcdf3bf69e1c67e8ac44fbe69
DaniVizel <78574752+DaniVizel@users.noreply.github.com>
2021-03-04 16:46:13 -0500

    Adding JQuery animated interaction... not working

---
+++ JS/jquery.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    $(".plant_1").mouseenter(function() {
+        $(".plant_1").animate({
+            height: '+=100px',
+            width: '+=100px',
+        });
+    });
+    $(".plant_1").mouseleave(function() {
+        $(".plant_1").animate({
+            height: '-=100px',
+            width: '-=100px',
+        });
+    });
+
+    $(".seed").mouseenter(function() {
+        $(".seed").animate({
+        height: '+=20px',
+        width: '+=20px',
+        });
+    });
+    $(".seed").mouseleave(function() {
+        $(".seed").animate({
+            height: '-=20px',
+            width: '-=20px',
+        });
+    });
+    $(".blank").hover(function() {
+        $(".plant_bounce").effect( "bounce", {distance:50px, }, slow );
+        });
+    });
+
+
+});
